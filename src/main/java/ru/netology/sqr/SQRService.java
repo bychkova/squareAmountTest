ec6c80813695a056eb8834bc8676a4654b3f30df
Maria Bychkova <mar.byckova@gmail.com>
2022-03-31 19:15:05 +0300

    Added square method

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,4 +1,14 @@
 package ru.netology.sqr;
 
 public class SQRService {
+    public int squareAmount (int from, int to){
+        int result = 0;
+        for (int i = 10; i <= 99; i++){
+            int square = i * i;
+            if (square >= from && square <= to){
+                result++;
+            }
+        }
+        return result;
+    }
 }
